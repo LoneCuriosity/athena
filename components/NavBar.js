94a2ba490dedd4e9d0e635b2cee6484a14a6e762
LoneCuriosity <ramon.garcia08@utrgv.edu>
2023-01-18 00:14:20 -0600

    first

---
+++ components/NavBar.js
@@ -0,0 +1,9 @@
+function NavBar() {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default NavBar;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import landing_icon from "./169.svg"
+import icon from "./icon.svg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="flex flex-row p-2 gap-2 items-center">
+        <img src={icon}></img>
+        <p className="font-Orbitron uppercase text-6xl text-stone-700">athena</p>
+      </div>
+      <img src={landing_icon}></img>
     </div>
   );
 }
